b6069d8e77432b964f758dda90103b6076be1d8d
pierrebrd <pierrebrd7@gmail.com>
2025-06-16 16:34:26 +0200

    Format launch file

--- launch/stage.launch.py
+++ launch/stage.launch.py
@@ -11,54 +11,64 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    this_directory = get_package_share_directory('stage_ros2')
+    this_directory = get_package_share_directory("stage_ros2")
 
     stage_world_arg = DeclareLaunchArgument(
-        'world',
-        default_value=TextSubstitution(text='cave'),
-        description='World file relative to the project world file, without .world')
+        "world",
+        default_value=TextSubstitution(text="cave"),
+        description="World file relative to the project world file, without .world",
+    )
 
-
-    enforce_prefixes = LaunchConfiguration('enforce_prefixes')
+    enforce_prefixes = LaunchConfiguration("enforce_prefixes")
     enforce_prefixes_arg = DeclareLaunchArgument(
-        'enforce_prefixes',
-        default_value='false',
-        description='on true a prefixes are used for a single robot environment')
-    
-    use_static_transformations = LaunchConfiguration('use_static_transformations')
-    use_static_transformations_arg = DeclareLaunchArgument(
-        'use_static_transformations',
-        default_value='true',
-        description='Use static transformations for sensor frames!')
+        "enforce_prefixes",
+        default_value="false",
+        description="on true a prefixes are used for a single robot environment",
+    )
 
-    one_tf_tree = LaunchConfiguration('one_tf_tree')
+    use_static_transformations = LaunchConfiguration("use_static_transformations")
+    use_static_transformations_arg = DeclareLaunchArgument(
+        "use_static_transformations",
+        default_value="true",
+        description="Use static transformations for sensor frames!",
+    )
+
+    one_tf_tree = LaunchConfiguration("one_tf_tree")
     one_tf_tree_arg = DeclareLaunchArgument(
-        'one_tf_tree',
-        default_value='false',
-        description='on true all tfs are published with a namespace on /tf and /tf_static')
-    
+        "one_tf_tree",
+        default_value="false",
+        description="on true all tfs are published with a namespace on /tf and /tf_static",
+    )
+
+    # TODO: add log level
+
     def stage_world_configuration(context):
         file = os.path.join(
-            this_directory,
-            'world',
-            context.launch_configurations['world'] + '.world')
-        return [SetLaunchConfiguration('world_file', file)]
+            this_directory, "world", context.launch_configurations["world"] + ".world"
+        )
+        return [SetLaunchConfiguration("world_file", file)]
 
     stage_world_configuration_arg = OpaqueFunction(function=stage_world_configuration)
 
-    return LaunchDescription([
-        stage_world_arg,
-        one_tf_tree_arg, 
-        enforce_prefixes_arg, 
-        use_static_transformations_arg, 
-        stage_world_configuration_arg,
-        Node(
-            package='stage_ros2',
-            executable='stage_ros2',
-            name='stage',
-            parameters=[{'one_tf_tree': one_tf_tree,
-                        'enforce_prefixes': enforce_prefixes,
-                        'use_static_transformations': use_static_transformations,
-                "world_file": [LaunchConfiguration('world_file')]}],
-        )
-    ])
+    return LaunchDescription(
+        [
+            stage_world_arg,
+            one_tf_tree_arg,
+            enforce_prefixes_arg,
+            use_static_transformations_arg,
+            stage_world_configuration_arg,
+            Node(
+                package="stage_ros2",
+                executable="stage_ros2",
+                name="stage",
+                parameters=[
+                    {
+                        "one_tf_tree": one_tf_tree,
+                        "enforce_prefixes": enforce_prefixes,
+                        "use_static_transformations": use_static_transformations,
+                        "world_file": [LaunchConfiguration("world_file")],
+                    }
+                ],
+            ),
+        ]
+    )
